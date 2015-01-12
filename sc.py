dd07ba84f495ce47796a57180342790b2d17c440
ddddddeon <opfunk@gmail.com>
2015-01-12 16:57:05 -0500

    strip characters that might fuck up a unix filesystem

--- sc.py
+++ sc.py
@@ -4,6 +4,7 @@ import requests
 from unidecode import unidecode
 
 CLIENT_ID = 'STUFF GOES HERE'
+
 username = sys.argv[1]
 
 client = soundcloud.Client(client_id=CLIENT_ID)
@@ -12,9 +13,9 @@ uid = user[0].obj['id']
 tracks = client.get('/users/' + str(uid) + '/tracks')
 for t in tracks:
     filename = unidecode(t.obj['title']) + '.mp3'
-    filename = filename.replace("/", "-")
+    filename = filename.strip("/*~\\$")
     f = open(filename, 'wb');
-    print "Saving: %s (%s)" % (t.obj['title'], t.obj['stream_url'])
+    print "[SAVING] %s -> %s" % (t.obj['title'], filename)
     stream_url = t.obj['stream_url']
     stuff = requests.get(stream_url + '?client_id=' + CLIENT_ID, allow_redirects=True)
     for chunk in stuff.iter_content(chunk_size=512 * 1024): 
