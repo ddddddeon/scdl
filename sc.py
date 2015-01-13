5273c29bb6fd4c6434a0c35071f79f1d9d805f51
george <chris@sweetiq.com>
2015-01-12 22:13:37 -0500

    add comments

--- sc.py
+++ sc.py
@@ -5,10 +5,22 @@ import sys
 import requests
 from unidecode import unidecode
 
+# you will need to set an environment variable with
+# your soundcloud client_id from https://developers.soundcloud.com
 CLIENT_ID = os.getenv('SOUNDCLOUD_CLIENT_ID')
+
 usernames = sys.argv[1::]
 client = soundcloud.Client(client_id=CLIENT_ID)
 
+def bail_out(_file, filename):
+    _file.close()
+    os.remove(filename)
+    print '  :('
+
+# call soundcloud api to get user ids, 
+# make directories for usernames,
+# call api again to get list of trax,
+# then GET all the trax & write blobs to disk
 for username in usernames:
     user = client.get('/users', q=username)
     try:
@@ -17,19 +29,17 @@ for username in usernames:
     except:
         print 'username not found :('
         continue
-
-    def bail_out(_file, filename):
-        _file.close()
-        os.remove(filename)
-        print '  :('
-
+    
     if not os.path.exists(username):
         os.makedirs(username)
-
+    
     for t in tracks:
+        # remove any characters that might fuck up a unix box
         filename = unidecode(t.obj['title']) + '.mp3'
         filename = re.sub("[/\*\~\\$]", "", filename)
         filename = username + '/' + filename
+
+        # make the file <username>/<trak>.mp3 
         f = open(filename, 'wb');
         sys.stdout.write("[SAVING] %s -> %s" % (t.obj['title'], filename))
         sys.stdout.flush()
@@ -39,14 +49,18 @@ for username in usernames:
         except KeyError:
             bail_out()
             continue
+        
+        # getting shady now...
+        # GET the trak's stream url w/ our client_id >:D
         try:
             stuff = requests.get(stream_url + '?client_id=' + CLIENT_ID, allow_redirects=True)
         except:
             bail_out()
             continue
 
+        # ...and write it to a file XD XD 
         for chunk in stuff.iter_content(chunk_size=512 * 1024): 
             if chunk:
                 f.write(chunk)
         f.close()
-        print u'  \u2713'
+        print u'  \u2713' # great job!
