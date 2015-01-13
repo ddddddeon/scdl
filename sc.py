17e3912da426a093310eaf9462185dbce29d0e47
george <chris@sweetiq.com>
2015-01-12 21:57:58 -0500

    flesh out absolutely everything

--- sc.py
+++ sc.py
@@ -1,26 +1,52 @@
 import soundcloud
 import re
+import os
 import sys
 import requests
 from unidecode import unidecode
 
-CLIENT_ID = 'STUFF GOES HERE'
-
-username = sys.argv[1]
-
+CLIENT_ID = os.getenv('SOUNDCLOUD_CLIENT_ID')
+usernames = sys.argv[1::]
 client = soundcloud.Client(client_id=CLIENT_ID)
-user = client.get('/users', q=username)
-uid = user[0].obj['id']
-tracks = client.get('/users/' + str(uid) + '/tracks')
-for t in tracks:
-    filename = unidecode(t.obj['title']) + '.mp3'
-    filename = filename.strip("\/*~\\$")
-    filename = re.sub("[/\*\~\\$]", "", filename)
-    f = open(filename, 'wb');
-    print "[SAVING] %s -> %s" % (t.obj['title'], filename)
-    stream_url = t.obj['stream_url']
-    stuff = requests.get(stream_url + '?client_id=' + CLIENT_ID, allow_redirects=True)
-    for chunk in stuff.iter_content(chunk_size=512 * 1024): 
-        if chunk:
-            f.write(chunk)
-    f.close()
+
+for username in usernames:
+    user = client.get('/users', q=username)
+    try:
+        uid = user[0].obj['id']
+        tracks = client.get('/users/' + str(uid) + '/tracks')
+    except:
+        print 'username not found :('
+        exit(1)
+
+    def bail_out(_file, filename):
+        _file.close()
+        os.remove(filename)
+        print '  :('
+
+    if not os.path.exists(username):
+        os.makedirs(username)
+
+    for t in tracks:
+        filename = unidecode(t.obj['title']) + '.mp3'
+        filename = re.sub("[/\*\~\\$]", "", filename)
+        filename = username + '/' + filename
+        f = open(filename, 'wb');
+        sys.stdout.write("[SAVING] %s -> %s" % (t.obj['title'], filename))
+        sys.stdout.flush()
+
+        try:
+            stream_url = t.obj['stream_url']
+        except KeyError:
+            bail_out()
+            continue
+        try:
+            stuff = requests.get(stream_url + '?client_id=' + CLIENT_ID, allow_redirects=True)
+        except:
+            bail_out()
+            continue
+
+        for chunk in stuff.iter_content(chunk_size=512 * 1024): 
+            if chunk:
+                f.write(chunk)
+        f.close()
+        print u'  \u2713'
